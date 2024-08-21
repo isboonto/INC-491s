d7c60e81c4936cabea136dfb56e20ffb4c108ce5
isboonto <59634198+isboonto@users.noreply.github.com>
2024-08-21 16:59:22 +0700

    Add files via upload

---
+++ ch3/julia/bracket_minimum.jl
@@ -0,0 +1,20 @@
+# from  algorithms for Optimization book pp 36 
+# Algorithm 3.1
+function bracket_minimum(f, x=0; s=1e-2, k=2.0)
+	a, ya = x, f(x)
+	b, yb = a + s, f(a + s)
+	
+	if yb > ya 
+		a, b = b, a 		# swap
+		ya, yb = yb , ya
+		s = -s
+	end
+	while true
+		c, yc = b + s, f(b + s)
+		if yc > yb
+			return a < c ? (a, c) : (c, a)
+		end
+		a, ya, b, yb = b, yb, c, yc
+		s *= k
+	end
+end
