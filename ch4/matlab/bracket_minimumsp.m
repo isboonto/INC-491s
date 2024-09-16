203bf1b1dbb122512a2ca9d8686f25aa784a0b39
isboonto <59634198+isboonto@users.noreply.github.com>
2024-09-16 17:51:08 +0700

    new file
    new file

---
+++ ch4/matlab/bracket_minimumsp.m
@@ -0,0 +1,36 @@
+function [a, c] = bracket_minimumsp(f, x, s, k)
+% x: starting point 
+% s: step size 
+% k: gamma (weight multiplier) 
+
+  if nargin < 2
+    x = 0; s = 1e-2; k = 2.0;
+  elseif nargin < 3
+    s = 1e-2; k = 2.0;
+  elseif nargin < 4
+    k = 2.0;
+  end
+
+  a = x; ya = f(x); b = a + s; yb = f(a + s);
+
+  % change the direction 
+  if yb > ya
+      [a, b] = deal(b,a);
+      [ya, yb] = deal(yb, ya);
+      s = -s;
+  end
+  % yb < ya 
+  flag = 1;
+  while flag
+    s = s * k;
+    c = b + s; yc = f(b + s);
+    if yc < yb
+        a = b; ya = yb; b = c; yb = yc;
+    else 
+        if c < a
+            [a, c] = deal(c,a);
+        end
+        flag = 0;
+    end
+  end
+end
