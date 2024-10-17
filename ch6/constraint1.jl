a4c9270738388ab606dd635e86b58015203620b0
isboonto <59634198+isboonto@users.noreply.github.com>
2024-10-18 00:01:44 +0700

    Update constraint1.jl

--- ch6/constraint1.jl
+++ ch6/constraint1.jl
@@ -26,7 +26,7 @@ begin
 
 	xs = -2.5:0.01:5; ys = -2.5:0.01:5
 	lev = collect(-10:1:30)
-	contour!(ax1, xs, ys, f, levels = lev, color=(:blue, 0.3))
+	contour!(ax1, xs, ys, f, levels = lev, colormap=:jet)#(:blue, 0.3))
 	#contour!(ax1, xs, ys, g1, levels = [0], linewidth = 2, color=:red, label=L"$g_1(\mathbf{x})$", labelcolor=:red)
 	#contour!(ax1, xs, ys, g2, levels = [0], linewidth = 2, color=:green, label=L"$g_2(\mathbf{x})$", labelcolor=:green)
 
@@ -52,7 +52,7 @@ begin
 	limits!(ax1, -2.5, 5, -2.5, 5)
 
 	scatter!(ax1, [1.05],[2.1], marker=:circle, markersize=12, color=:blue, strokecolor=(:black, 0.5), strokewidth=2)
-	text!(ax1, [0.9], [2.3], text=L"$x^\ast$", color=:black)
+	text!(ax1, [0.9], [2.3], text=L"$\mathbf{x^\ast}$", color=:black)
 
 	axislegend(ax1,fontsize=18)
 	fig1
@@ -63,9 +63,29 @@ begin
 	save("/mnt/e/OneDrive/Public/workKMUTT/INC Selection Optimization/Lecture2022/images/constrain1.pdf", fig1)
 end
 
+# ╔═╡ b607a3a1-e22b-43c6-87de-7c92306654bd
+begin
+	f4b(x1, x2, x3) = x1^2 + x2^2 - 2x1
+	f4a(x1, x2, x3) = -x1 + x3 - 1
+end
+
+# ╔═╡ 614f6e60-98b5-4699-94d0-105553e6a62d
+begin
+	fig2 = Figure(fontsize=18)
+	ax2 = Axis3(fig2[1,1], xlabel=L"$x_1$", ylabel=L"$x_2$", zlabel=L"$x_3$")
+
+	xp = 0:0.01:2π
+	x1 = 1 .+ cos.(xp); x2 = sin.(xp); x3 = 2 .+ cos.(xp)
+	lines!(ax2, x1, x2, x3, linewidth=2)
+	
+	fig2
+end
+
 # ╔═╡ Cell order:
 # ╠═2f4683f8-8c5e-11ef-3ff0-7107adf83b3c
 # ╠═c18a845b-8c9d-41da-a528-fa9b6d60be90
 # ╠═89119540-f1ae-4c6c-a184-4a509081fa65
 # ╠═31fdb1ba-e671-4ae7-bf95-705dcb9a5a7c
 # ╠═14ce9e57-ca9f-46e9-a979-bcc2cd850d94
+# ╠═b607a3a1-e22b-43c6-87de-7c92306654bd
+# ╠═614f6e60-98b5-4699-94d0-105553e6a62d
