d3add5434667f4b5087c7ac95c334b11c4d87fe2
isboonto <59634198+isboonto@users.noreply.github.com>
2024-10-25 00:25:18 +0700

    new file for lecture note

---
+++ ch6/eq_con1.jl
@@ -0,0 +1,102 @@
+### A Pluto.jl notebook ###
+# v0.20.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 60eeca4c-904a-11ef-1de9-c3667533212e
+begin
+	using Pkg; Pkg.activate()
+end
+
+# ╔═╡ 6373d11c-92a9-434a-95a6-f405b1731d2d
+begin
+	using ForwardDiff 
+	using LaTeXStrings
+	using CairoMakie
+		set_theme!(theme_latexfonts(), fontsize=18)
+	using LinearAlgebra
+end
+
+# ╔═╡ 6d9c1bb4-81fa-4883-93da-64b36ebffd57
+begin
+	f(x) = x[1] + 2x[2] 
+	h(x) = (1/4)*x[1]^2 + x[2]^2 - 1;
+	#hx2 = x -> sqrt(1 - (1/4)*x^2)
+	
+	∇f(x)= ForwardDiff.gradient(f, x)
+	f1(x,y) = f([x,y])
+	h1(x,y) = h([x,y])
+	gf(x,y) = ∇f([x,y])
+end
+
+# ╔═╡ 44e8859d-6bee-450e-be2c-77e6570131fc
+gf(-1,1)'*[1,1]
+
+# ╔═╡ 146d5f22-62d4-4876-9fb5-92694997a554
+begin
+	x1s = LinRange(-3,4, 50); x2s = LinRange(-3,3, 50); 
+
+	xv = [-1,1]
+	
+	z = [f1(x,y) for x in x1s, y in x2s];
+	zl = [f1(-1,1) + gf(-1,1)'*[x,y] for x in x1s, y in x2s ]
+	
+	
+	fig = Figure(size = (400,400))
+	ax = Axis(fig[1,1], xlabel = L"x_1", ylabel = L"x_2", 
+		aspect = AxisAspect(1.4))
+	hidexdecorations!(ax, ticks=false, grid=true)  # hides ticks, grid and labels
+	hideydecorations!(ax, ticks=false)  # hides ticks, grid and lables
+	#hidespines!(ax)  # hide the frame
+	
+	contour!(ax,x1s,x2s,z, levels= -20:1:20, color=(:blue, 0.4), linewidth =1)
+	contour!(ax,x1s,x2s,zl, levels = [9], color=(:blue), linewidth=3, 
+		linestyle=:solid)
+	contour!(ax,x1s,x2s,h1, levels=[0], color=(:red, 0.3), linewidth=2)
+	limits!(ax, -3,3,-3, 3)
+
+	scatter!([-1],[1], color=:blue)
+	yy = x -> (1.75-0.5)/(0.5-(-2.0))*x + 1.5  # offset
+	
+	#band!(ax, x1s, yy.(x1s), 0.5*ones(length(x1s)), color=(:cyan, 0.2))
+	fig[1,1] = ax
+	
+
+	
+	pv = [1/√5, 2/√5] # steepest direction is -g 
+	α = 1
+	cosv = xv'*pv / (norm(xv)*norm(pv))
+	alength = xv'*pv/norm(pv)
+	g1 = gf(xv[1],xv[2])*0.1
+
+	
+	text!(ax, L"\nabla f^T\mathbf{p}  = 0", position=(-0.45,1.15), color=:blue)
+	
+	arrows!(ax, [xv[1]], [xv[2]], [g1[1]], [g1[2]], lengthscale = 1, 
+		color=:black, linewidth=2, arrowsize=15)
+	text!(ax, L"\nabla f", position=(-1.4,1.4), color=:black)
+	
+	arrows!(ax, [xv[1]], [xv[2]], 0.5*[pv[1]], [pv[2]], lengthscale = 0.5,
+		color=:red, linewidth=2, arrowsize=15)
+	text!(ax, L"\nabla f^T\mathbf{p} > 0", position=(-0.85,1.5), color=:red)
+
+	arrows!(ax, [xv[1]], [xv[2]], [-1], [-1.5], lengthscale = 0.3, 
+		color=:red, linewidth=2, arrowsize=15)
+	text!(ax, L"\nabla f^T\mathbf{p} < 0", position=(-1.2,0.55), color=:red)
+
+	
+	
+	cd("/mnt/e/OneDrive/Public/workKMUTT/INC Selection Optimization/Lecture2022/images/")
+	
+	save("first_con1b.pdf", fig)
+	run(`pdfcrop  --clip first_con1b.pdf first_con1.pdf`)
+	fig
+end
+
+# ╔═╡ Cell order:
+# ╠═60eeca4c-904a-11ef-1de9-c3667533212e
+# ╠═6373d11c-92a9-434a-95a6-f405b1731d2d
+# ╠═6d9c1bb4-81fa-4883-93da-64b36ebffd57
+# ╠═44e8859d-6bee-450e-be2c-77e6570131fc
+# ╠═146d5f22-62d4-4876-9fb5-92694997a554
